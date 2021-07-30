107b32bc40a0c108499602b1f36feed9e6eb8c10
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-30 17:47:07 -0300

    JWT Refresh Token

--- src/main/java/me/gabreuw/springsecurityjwt/api/UserResource.java
+++ src/main/java/me/gabreuw/springsecurityjwt/api/UserResource.java
@@ -1,5 +1,10 @@
 package me.gabreuw.springsecurityjwt.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import me.gabreuw.springsecurityjwt.domain.AppUser;
 import me.gabreuw.springsecurityjwt.domain.Role;
@@ -9,8 +14,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -61,6 +77,59 @@ public class UserResource {
         return ResponseEntity
                 .noContent()
                 .build();
+    }
+
+    @GetMapping(path = "/token/refresh")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            throw new RuntimeException("Refresh token is missing.");
+        }
+
+        try {
+            String refresh_token = authorizationHeader.substring("Bearer ".length());
+
+            Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+            JWTVerifier verifier = JWT.require(algorithm).build();
+
+            DecodedJWT decodedJWT = verifier.verify(refresh_token);
+
+            String username = decodedJWT.getSubject();
+            AppUser appUser = USER_SERVICE.getUser(username);
+
+            String access_token = JWT.create()
+                    .withSubject(appUser.getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1_000))
+                    .withIssuer(request.getRequestURI())
+                    .withClaim(
+                            "roles",
+                            appUser.getRoles().stream()
+                                    .map(Role::getName)
+                                    .collect(Collectors.toList())
+                    )
+                    .sign(algorithm);
+
+            Map<String, String> tokens = new HashMap<>() {{
+                put("access_token", access_token);
+                put("refresh_token", refresh_token);
+            }};
+
+            response.setContentType(APPLICATION_JSON_VALUE);
+            new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+        } catch (Exception e) {
+            Map<String, String> tokens = new HashMap<>() {{
+                put("error", e.getMessage());
+            }};
+
+            response.setContentType(APPLICATION_JSON_VALUE);
+            response.setStatus(FORBIDDEN.value());
+            new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+        }
     }
 
 }
